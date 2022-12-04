d7f3e94c972bda9422f5c666337eebebc0611bb6
Marcus <marcus@springbok.ai>
2022-12-04 19:43:25 +0000

    day 4 part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -10,6 +10,23 @@ with open('./Day4/input.txt') as file:
     ranges = [[int(r) for r in a.split("-")] for a in assignments]
     isContainingPair = isFullyContainingPair(ranges)
     containingPairs = containingPairs + 1 if isContainingPair else containingPairs
-    print(f"{ranges}: {isContainingPair}")
     
 print(f"There are {containingPairs} pairs that are fully containing")
+
+# Part 2
+
+def hasOverlap(ranges):
+  ranges.sort(key=lambda r: r[0])
+  return ranges[0][1] >= ranges[1][0] or ranges[1][1] <= ranges[0][0]
+
+overlappingPairs = 0
+with open('./Day4/input.txt') as file:
+  for line in file:
+    assignments = line.strip().split(",")
+
+    ranges = [[int(r) for r in a.split("-")] for a in assignments]
+    isOverlappingPair = hasOverlap(ranges)
+    overlappingPairs = overlappingPairs + 1 if isOverlappingPair else overlappingPairs
+    print(f"{ranges}: {isOverlappingPair}")
+    
+print(f"There are {overlappingPairs} overlapping pairs")
