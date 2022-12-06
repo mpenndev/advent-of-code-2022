89a26e992b572ef79aa29a3c2280f7c561fbd6eb
Marcus <marcus@springbok.ai>
2022-12-06 09:10:28 +0000

    day 5 part 1

--- Day5/day5.py
+++ Day5/day5.py
@@ -2,15 +2,15 @@ from typing import List
 
 def parseStartingPosition(positionInput: List[str]) -> List[List[str]]:
   parsedPosition = []
-  # TODO use index of numbers from bottom or sequence
-  for index in [1, 5, 9]:
-    parsedPosition.append([c[index] for c in positionInput[:-1] if c[index] != " "])
+  finalIndex = 4 * int(positionInput[-1].strip()[-1]) - 3 + 4
+  for i in range(1, finalIndex, 4):
+    parsedPosition.append([c[i] for c in positionInput[:-1] if c[i] != " "])
   return parsedPosition
 
 def parseInstructions(instructionsInput: List[str]) -> List[List[int]]:
   parsedInstructions = []
   for instruction in instructionsInput:
-    parsedInstructions.append([int(c) for c in instruction if c.isdigit()])
+    parsedInstructions.append([int(c) for c in instruction.split() if c.isdigit()])
   return parsedInstructions
 
 def runInstructions(startingPosition: List[List[str]], instructions: List[List[int]]):
@@ -19,7 +19,7 @@ def runInstructions(startingPosition: List[List[str]], instructions: List[List[i
     stackToMoveFrom = startingPosition[instruction[1] - 1]
     stackToMoveTo = startingPosition[instruction[2] - 1]
 
-    for i in range(instruction[0]):
+    for _ in range(instruction[0]):
       crate = stackToMoveFrom.pop(0)
       stackToMoveTo.insert(0, crate)
   return position
@@ -35,4 +35,4 @@ startingPosition = parseStartingPosition(positionInput)
 instructions = parseInstructions(instructionsInput)
 
 outcome = runInstructions(startingPosition, instructions)
-print([stack[0] for stack in outcome])
+print(''.join([stack[0] for stack in outcome]))
