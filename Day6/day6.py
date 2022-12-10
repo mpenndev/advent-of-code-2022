05f4febd9972ea507f75878c53833ff72da899b4
Marcus <marcus@springbok.ai>
2022-12-10 11:43:50 +0000

    day 6 part 2

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,8 +1,15 @@
+def getUniqueCharactersIndex(input: str, requiredUniqueCharacters: int):
+  for i in range(requiredUniqueCharacters, len(input)):
+    if len(set(input[i-requiredUniqueCharacters:i])) == requiredUniqueCharacters:
+      return i
+
 with open("./Day6/input.txt") as file:
   input = file.read()
 
-for i in range(4, len(input)):
-  if len(set(input[i-4:i])) == 4:
-    break
+startOfPacketIndex = getUniqueCharactersIndex(input, 4)
+print(f"Start of packet: {startOfPacketIndex}")
 
-print(i)
+# Part 2
+
+startOfMessageIndex = getUniqueCharactersIndex(input, 14)
+print(f"Start of message: {startOfMessageIndex}")
