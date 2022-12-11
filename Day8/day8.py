e46714c14ccbfa07e96c840ac8f24b552af21123
Marcus <marcus@springbok.ai>
2022-12-11 09:45:45 +0000

    day 8 part 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,7 +1,7 @@
 from typing import List
 
-def getEdgeCount(map: List[List[int]]):
-  return len(map) * 2 + len(map[0]) * 2 - 4
+def getVerticalPlane(map: List[List[int]], x: int):
+  return [map[i][x] for i in range(0, len(map))]
 
 def isVisibleInPlane(planeSection: List[int], currentPos: int):
   height = planeSection[currentPos]
@@ -18,6 +18,9 @@ def isVisibleInPlane(planeSection: List[int], currentPos: int):
 
   return True
 
+def getEdgeCount(map: List[List[int]]):
+  return len(map) * 2 + len(map[0]) * 2 - 4
+
 map = []
 with open('./Day8/input.txt') as file:
   for line in file:
@@ -26,7 +29,44 @@ with open('./Day8/input.txt') as file:
 innerCount = 0
 for y in range(1, len(map) - 1):
   for x in range(1, len(map[0]) - 1):
-    if isVisibleInPlane(map[y], x) or isVisibleInPlane([map[i][x] for i in range(0, len(map))], y):
+    if isVisibleInPlane(map[y], x) or isVisibleInPlane(getVerticalPlane(map, x), y):
       innerCount += 1
 
 print(f"Visible trees: {getEdgeCount(map) + innerCount}")
+
+# Part 2
+
+def getDirectionScenicScore(planeSection: List[int], currentPos: int, step: int, condition):
+  height = planeSection[currentPos]
+  count = 0
+  i = currentPos + step
+  blocked = False
+  
+  while not blocked and condition(i):
+    count += 1
+    if (planeSection[i] >= height):
+      blocked = True
+    i += step
+
+  return count
+
+def calculateScenicScore(map: List[List[int]], x: int, y: int):
+  horizontalPlane = map[y]
+  verticalPlane = getVerticalPlane(map, x)
+  negativeCondition = lambda i: i >= 0
+
+  xMinusScore = getDirectionScenicScore(horizontalPlane, x, -1, negativeCondition)
+  xPlusScore = getDirectionScenicScore(horizontalPlane, x, 1, lambda i: i < len(horizontalPlane))
+  yMinusScore = getDirectionScenicScore(verticalPlane, y, -1, negativeCondition)
+  yPlusScore = getDirectionScenicScore(verticalPlane, y, 1, lambda i: i < len(verticalPlane))
+  
+  return xMinusScore * xPlusScore * yMinusScore * yPlusScore
+
+highestScore = 0
+for y in range(1, len(map) - 1):
+  for x in range(1, len(map[0]) - 1):
+    score = calculateScenicScore(map, x, y)
+    if score > highestScore:
+      highestScore = score
+  
+print(f"Highest scenic score: {highestScore}")
